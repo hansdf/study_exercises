56f5c9eeebbaab7c2ca1f5695a7ed7d2e082a558
hansdf <dornsback@gmail.com>
2025-02-04 22:49:05 -0300

    update merge lists problem

--- merge_list.py
+++
@@ -1,8 +0,0 @@
-# Merge Two Sorted Lists: Given two sorted lists, merge them into one sorted list.
-
-nums1 = [1, 3, 5, 7, 9]
-num2 = [2, 4, 6, 8, 10]
-
-def merge_lists(list1, list2):
-    
-    pass
---
+++ merge_lists.py
@@ -0,0 +1,17 @@
+# Merge Two Sorted Lists: Given two sorted lists, merge them into one sorted list.
+
+nums1 = [1, 3, 5, 7, 9]
+nums2 = [2, 4, 6, 8, 10]
+
+def merge_lists(list1, list2): # very poor optimization, but works
+    combined_list = []
+    for num in list1:
+        combined_list.append(num)
+
+    for num in list2:
+        combined_list.append(num)
+    
+    combined_list.sort()
+    print(combined_list)
+
+merge_lists(nums1, nums2)

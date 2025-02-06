c794aae16acbc905b1c618e0543778376d8e39a4
Hans <dornsback@gmail.com>
2025-02-05 22:18:45 -0300

    needle in a haystack problem solution

--- needle_haystack.py
+++ needle_haystack.py
@@ -1,9 +1,15 @@
 # Leet code 28. Find the Index of the First Occurrence in a String
-
 # Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
+needle_string = "but"
+haystack_string = "sadbutsad"
+
 def find_needle(needle, haystack):
-    index = 0
-    for letter in haystack:
+    needle_len = len(needle)
+
+    for i in range(len(haystack)):
+        # print(haystack[i:i+needle_len])
+        if haystack[i:i+needle_len] == needle:
+            print(f"{i} is the index of where the word {needle} starts")
         
-        pass
+find_needle(needle_string, haystack_string)

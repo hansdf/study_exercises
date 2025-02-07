5d874dc29dd4c79de352a92a94cb5f390ff25861
Hans <dornsback@gmail.com>
2025-02-07 19:21:07 -0300

    search algo study

--- search_algo.py
+++ search_algo.py
@@ -1,23 +1,34 @@
 # linear and binary search algorithms test on sorted lists
 
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+nums = [0, 1, 2, 15, 4, 5, 6, 7, 8, 9, 10]
 
-def linear_find(numlist, target):
-    pass
+def linear_find(numlist, target): # iterate entire list
+    index = 0
+    for n in numlist:
+        if n == target:
+            print("found target at index", index)
+        index += 1
     
 
 def binary_find(numlist, target):
+    first_item = 0
+    last_item = len(numlist) - 1
+
     for n in range(1, len(numlist)):
-        middle_n = int(len(numlist)/2) # index of middle element
-        print("middlenum: ", middle_n)
-        print("numlist[middle_n]: ", numlist[middle_n])
+        middle_n = len(numlist) // 2 # index of middle element
+
         if numlist[middle_n] == target:
             return print(f"found target: {target} at index: {numlist[middle_n]}")
         elif numlist[middle_n] < target:
-            pass
+            print(f"Queried number {numlist[middle_n]} is smaller than target")
+            middle_n = int(middle_n/2) # update index to check in next iteration
+            print("new middle_n is: ", middle_n)
         elif numlist[middle_n] > target:
-            pass
+            print(f"Queried number {numlist[middle_n]} is bigger than target")
+            middle_n = int(middle_n/2) # update index to check in next iteration
+            print("new middle_n is: ", middle_n)
         
-        middle_n = middle_n/2
+        
+# binary_find(nums, 9)
 
-binary_find(nums,4)
+linear_find(nums, 15)

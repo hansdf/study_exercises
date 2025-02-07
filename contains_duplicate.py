1f6f65554c734dbcf4a9012c436177b194381903
Hans <dornsback@gmail.com>
2025-02-07 16:14:06 -0300

    dups / sumof even updt

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -1,8 +1,8 @@
 # Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
 
-nums = [1, 2, 3, 4, 5, 5]
+nums = [1, 2, 3, 3, 3, 3, 4, 4, 5, 6, 7]
 
-#bruteforce solution = O(n^2)
+# bruteforce solution = O(n^2)
 def check_dupli(numlist):
     for n in numlist:
         counter = 0
@@ -13,4 +13,19 @@ def check_dupli(numlist):
                 return True
     return False
 
-print(check_dupli(nums))
+# hashmap approach = O(n)?
+def hashmap_check_dupli(numlist):
+    hash_counter = {}
+    for n in numlist:
+        if n not in hash_counter:
+            hash_counter[n] = 1
+        else:
+            hash_counter[n] += 1
+    for key in hash_counter:
+        # print(key, "->", hash_counter[key])
+        if hash_counter[key] > 1:
+            return True
+    return False
+
+
+print(hashmap_check_dupli(nums))
--- sum_of_even.py
+++ sum_of_even.py
@@ -7,4 +7,17 @@ def sum_of_even(max):
             sum_counter += num
     print(sum_counter)
 
-sum_of_even(100)
+# sum_of_even(100)
+
+# Sum of Even Numbers in a List
+# Problem: Return the sum of all even numbers in a given list.
+
+def sum_even_list(nums):
+    sums = 0
+    for n in nums:
+        pass
+
+
+numlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11]
+
+sum_even_list(numlist)

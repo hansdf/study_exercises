cb360bf076ae5d52947e030782098cb13cd103ee
Hans <dornsback@gmail.com>
2025-02-10 09:43:00 -0300

    Create group_anagrams.py

---
+++ group_anagrams.py
@@ -0,0 +1,23 @@
+# Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+# An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+strs = ["act","pots","tops","cat","stop","hat"] # Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+
+def group_anagram(wordlist):
+    anagram_hash = {}
+    for word in strs:
+        sorted_word = sorted(word)
+        sortagain = "".join(sorted_word)
+        if sortagain not in anagram_hash:
+            anagram_hash[sortagain] = [word]
+        else:
+            anagram_hash[sortagain].append(word)
+    group_list = []
+    for key in anagram_hash:
+        if key not in group_list:
+            group_list.append(anagram_hash[key])
+    print(group_list)
+    return group_list
+
+
+group_anagram(strs)
